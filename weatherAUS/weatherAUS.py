9640a345aa0a3648bd7830db15a90e298a999208
Luis Caballero <lacb0311@hotmail.com>
2022-04-29 00:43:07 -0500

    conversion de categorias str a int en weatherAUS

--- weatherAUS/weatherAUS.py
+++ weatherAUS/weatherAUS.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 28 22:18:21 2022
+import pandas as pd
+import numpy as np
 
-@author: Luis
-"""
+url = 'weatherAUS.csv'
+data2 = pd.read_csv(url)
+data2.drop(['Date', 'Sunshine', 'Evaporation', 'Humidity9am', 'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Temp9am', 'Temp3pm'], axis=1, inplace=True)
+data2.drop(['Location', 'MinTemp', 'MaxTemp', 'WindSpeed9am', 'WindSpeed3pm'], axis=1, inplace=True)
+
+data2.RainTomorrow.replace(['No', 'Yes'], [0, 1], inplace=True)
+data2.RainToday.replace(['No', 'Yes'], [0, 1], inplace=True)
+data2.WindGustDir.replace(['no', 'yes'], [0, 1], inplace=True)
+
 
